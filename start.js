7fa1541e04d1d515458675523d4c6a9d5ca750e3
MatteoBax <98881971+MatteoBax@users.noreply.github.com>
2022-08-23 15:09:20 +0200

    Create start.js

---
+++ start.js
@@ -0,0 +1,26 @@
+const { spawn } = require('child_process');
+const config = require('./config.json');
+function spawnNodemon() {
+  const cp = spawn('nodemon', [config.file, '--watch', config.pathToWatch], {
+    stdio: ['pipe', 'pipe', 'pipe', 'ipc'],
+  });
+
+  return cp;
+}
+
+var app = spawnNodemon();
+app.stderr.on('data', (data) => {
+  console.error(`${data}`);
+});
+app.stdout.on('data', (data) => {
+  console.error(`${data}`);
+});
+
+app.on('message', function (event) {
+  if (event.type === 'start') {
+  } else if (event.type === 'crash') {
+    app.send('restart');
+  }else if (event.type === 'exit') {
+    app.send('quit');
+  }
+});
